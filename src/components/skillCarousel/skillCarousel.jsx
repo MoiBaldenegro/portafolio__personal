f5d7e33bbe13bb5841c1e173faedb58439e86de6
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2023-08-04 05:58:10 +0200

    respaldado

--- src/components/skillCarousel/skillCarousel.jsx
+++ src/components/skillCarousel/skillCarousel.jsx
@@ -1,11 +1,14 @@
 import Carousel from "react-multi-carousel";
+import { Container, Row, Col } from "react-bootstrap";
 import "react-multi-carousel/lib/styles.css";
+import style from "./skillCarousel.module.css"
+
 
 export default function SkillCarousel(){
     
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
+        
           breakpoint: { max: 4000, min: 3000 },
           items: 5
         },
@@ -21,9 +24,44 @@ export default function SkillCarousel(){
           breakpoint: { max: 464, min: 0 },
           items: 1
         }
+      };
 
 
-    return(
+    return (
+            <section  className={ style.skills } >
+              <Container className={ style.skillsConatiner } >
+                <Row>
+                  <Col>
+                    <div>
+                      <h2>
+                        Skills
+                      </h2>
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro voluptas, aperiam iure ut fugit sequi ipsum asperiores magnam. Quod nihil rem maiores et nam id libero doloremque delectus quisquam repudiandae!
+                      </p>
+                      <Carousel responsive={ responsive }>
+                        <div>
+                          <img src="" alt=""/>
+                          <h5> JavaScript </h5>
+                        </div>
+                        <div>
+                          <img src="" alt=""/>
+                          <h5> React </h5>
+                        </div>
+                        <div>
+                          <img src="" alt=""/>
+                          <h5> Node JS </h5>
+                        </div>
+                        <div>
+                          <img src="" alt=""/>
+                          <h5> Redux </h5>
+                        </div>
+                      </Carousel>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+          </section>
 
     )
 };
